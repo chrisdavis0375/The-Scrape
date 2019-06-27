ba01c3ae34674f2efaa45fb538920b1b8260f19d
Chris Davis <chrisdavis0375@gmail.com>
2019-06-27 11:07:17 -0700

    Get and post routes made and comment section added, data still needs to be sent to articles route

--- public/js/app.js
+++ public/js/app.js
@@ -3,27 +3,77 @@ $.getJSON("/articles", function(data) {
     console.log("Articles retrieved.");
   }
 
-  for (var i = 4; i < data.length; i++) {
+  for (var i = 0; i < data.length; i++) {
     $(".articles-section").append(
       "<div class = 'row'>" +
         "<div class = 'col-sm-12 article-content-boxes'>" +
-        "<p>" +
+        "<h3 data-id=" +
+        data[i]._id +
+        ">" +
         data[i].title +
-        "</p>" +
+        "</h3>" +
         "<br />" +
         "<p>" +
         data[i].summary +
         "</p>" +
+        "<button>" +
+        "Save this article?" +
+        "</button>" +
         "</div>" +
         "</div>" +
-        // "<div class = 'row'>" +
-        // "<div class = 'col-sm-12' id = 'comment-section'>" +
-        // "<p class = 'comment-text'>Leave a comment here!" +
-        // "</p>" +
-        // "<textarea class = 'col-sm-8'>" +
-        // "</textarea>" +
-        // "</div>" +
-        "</div>"
+        "<div class = 'row'>" +
+        "<div class = 'col-sm-12' id = 'comment-section'>" +
+        +"</div>"
     );
   }
+});
+
+$(document).on("click", "h3", function() {
+  $("#notes").empty();
+  let thisId = $(this).attr("data-id");
+
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+  }).then(function(data) {
+    console.log(data);
+    // An input to enter a new title
+    $("#comment-section").append("<input id='titleinput' name='title' >");
+    // A textarea to add a new note body
+    $("#comment-section").append(
+      "<textarea id='bodyinput' name='body'></textarea>"
+    );
+    // A button to submit a new note, with the id of the article saved to it
+    $("#comment-section").append(
+      "<button data-id='" + data._id + "' id='save-comment'>Save Note</button>"
+    );
+
+    // If there's a note in the article
+    if (data.Comment) {
+      // Place the title of the note in the title input
+      $("#titleinput").val(data.Comment.title);
+      // Place the body of the note in the body textarea
+      $("#bodyinput").val(data.Comment.body);
+    }
+  });
+
+  // $("#bodyinput").val("");
+});
+
+$(document).on("click", "#save-comment", function() {
+  let thisId = $(this).attr("data-id");
+
+  $.ajax({
+    method: "POST",
+    url: "/articles/" + thisId,
+    data: {
+      title: $("#titleinput").val(),
+      body: $("#bodyinput").val()
+    }
+  }).then(function(data) {
+    console.log(data);
+    $("#comment-section").empty();
+  });
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
 });
--- routes/html-routes.js
+++
@@ -1,3 +0,0 @@
-//module.exports = app.get("/", function(req, res) {
-//res.render("index");
-//});
